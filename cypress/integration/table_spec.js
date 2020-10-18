a8f7f82eb6a20c765243141c50ec94f9ab25fe8a
christian-miljkovic <christian.miljkovic@gmail.com>
2020-10-17 21:22:16 -0400

    feat(test): getFriends complete

--- cypress/integration/table_spec.js
+++ cypress/integration/table_spec.js
@@ -1,4 +1,5 @@
 import { ENDPOINTS } from '../../src/api/endpoints';
+import { convertToCamelCase } from '../../src/util/transformers'
 import { v4 as uuidv4 } from 'uuid'
 
 const clientUUID = uuidv4()
@@ -6,11 +7,11 @@ const token = Cypress.env("REACT_APP_API_TOKEN")
 
 const hasFriendsPresent = (friends = []) => {
   friends.map((friend, index) => {
-    cy.findByLabelText(`row-${index}`).within(() => {
-      cy.findByLabelText('firstName-cell').contains(friend.first_name)
-      cy.findByLabelText('lastName-cell').contains(friend.last_name)
-      cy.findByLabelText('birthday-cell').contains(friend.birthday)
-      cy.findByLabelText('phoneNumber-cell').contains(friend.phone_number)
+    cy.findByText(friend.firstName)
+    cy.findByLabelText(`row-${index}`).within(() => {    
+      cy.findByLabelText(/lastname\-cell/i).contains(friend.lastName)
+      cy.findByLabelText(/birthday\-cell/i).contains(friend.birthday)
+      cy.findByLabelText(/phonenumber\-cell/i).contains(friend.phoneNumber)
     })
   })
   
@@ -21,14 +22,12 @@ describe('Friends Table', () => {
   it('should be able to get all friends for a client', () => {
     cy.server()
     cy.fixture('friendsData').as('friendsJson')
-    // have to do a custom polyfill to change fetch to XHR see amber
     cy.route(`${ENDPOINTS.FRIENDS.ALL.replace(':id', clientUUID)}?token=${token}`, '@friendsJson').as('friends')
     cy.visit(`/table/${clientUUID}`)
 
     cy.wait('@friends').then(({ response }) => {
-      // go convert the base request you're using to have camelCase next
       const friends = response?.body?.data
-      hasFriendsPresent(friends)
+      hasFriendsPresent(convertToCamelCase(friends))
     })
     
   });
